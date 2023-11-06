448346887a56e3e09aab71810a86c34a135244f0
Taesun 94 <tyzlddy@naver.com>
2023-11-07 00:02:31 +0900

    feat: Add prizes page

---
+++ src/pages/prizes/Prizes.jsx
@@ -0,0 +1,11 @@
+import * as styles from './Prizes.module.css';
+
+function Prizes() {
+  return (
+    <div className={styles.container}>
+      PRIZES
+    </div>
+  )
+}
+
+export default Prizes;

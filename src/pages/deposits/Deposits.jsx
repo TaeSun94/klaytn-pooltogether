d2d536189b80f4e309d21854009506a41bcf7c25
Taesun 94 <tyzlddy@naver.com>
2023-11-07 00:02:13 +0900

    feat: Add deposits page

---
+++ src/pages/deposits/Deposits.jsx
@@ -0,0 +1,11 @@
+import * as styles from './Deposits.module.css';
+
+function Deposits() {
+  return (
+    <div className={styles.container}>
+      DEPOSITS
+    </div>
+  )
+}
+
+export default Deposits;

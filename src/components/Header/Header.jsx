0d77fcd68e4935098fa4eab7cf5b6d7af8bce584
TaeSun94 <tyzlddy@naver.com>
2023-11-10 14:26:47 +0900

    feat: add header links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import * as styles from './Header.module.css';
 import { } from 'react';
 
@@ -9,7 +10,9 @@ function Header() {
           LOGO
         </div>
         <div className={styles.contents}>
-          Contents
+          <Link href="/deposits">Deposits</Link>
+          <Link href="/prizes">Prizes</Link>
+          <Link href="/accounts">Accounts</Link>
         </div>
         <div className={styles.subContents}>
           ConnectWallet
